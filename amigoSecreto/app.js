76051b803ff9d87dc1e6b938a606afc6ba66b8de
Jesusva07 <valencialeonjesusdavid@gmail.com>
2025-02-24 14:21:01 -0500

    Añadi las funciones agregarAmigo() y actualizarListaAmigos().

--- amigoSecreto/app.js
+++ amigoSecreto/app.js
@@ -1,1 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let amigoAgregado = "";
+console.log(amigoAgregado);
+
+function agregarAmigo() {
+    let amigoAgregado = document.getElementById("amigo").value;
+    if (amigoAgregado) {
+        amigos.push(amigoAgregado);
+        console.log(`Amigo agregado: ${amigoAgregado}`);
+        document.getElementById("amigo").value = "";
+    } else {
+        alert("No se ha ingresado ningún nombre.");
+        console.log("No se ha ingresado ningún nombre.");
+    }
+}
+
+function actualizarListaAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpiar la lista existente
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
